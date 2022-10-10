c87d8fb731254789716512f6f076baf42b4412f5
Mike Mulchrone <mtmulch0191@outlook.com>
2022-10-10 00:24:40 -0400

    adding generate-random-numbers

---
+++ generate-random-numbers/target/debug/deps/generate_random_numbers-8561d56324752b28.d
@@ -0,0 +1,5 @@
+/mnt/c/Code/Rust/Rust-Cookbook/generate-random-numbers/target/debug/deps/generate_random_numbers-8561d56324752b28: src/main.rs
+
+/mnt/c/Code/Rust/Rust-Cookbook/generate-random-numbers/target/debug/deps/generate_random_numbers-8561d56324752b28.d: src/main.rs
+
+src/main.rs:
---
+++ generate-random-numbers/target/debug/generate-random-numbers.d
@@ -0,0 +1,1 @@
+/mnt/c/Code/Rust/Rust-Cookbook/generate-random-numbers/target/debug/generate-random-numbers: /mnt/c/Code/Rust/Rust-Cookbook/generate-random-numbers/src/main.rs
