e460ab054008605a25005bd44af5205d44637473
Mike Mulchrone <mtmulch0191@outlook.com>
2022-10-12 15:20:54 -0400

    sort array.

---
+++ geeks-for-geeks-computer-science/src/array.rs
@@ -0,0 +1,4 @@
+pub fn sort_array(mut arrayToSort: Box<[i64]>) -> Box<[i64]> {
+    arrayToSort.sort();
+    return arrayToSort;
+}
--- geeks-for-geeks-computer-science/src/main.rs
+++ geeks-for-geeks-computer-science/src/main.rs
@@ -1,3 +1,15 @@
+use array::sort_array;
+
+mod array;
+
 fn main() {
-    println!("Hello, world!");
+    sort_i64_boxed_array();
+}
+
+fn sort_i64_boxed_array() {
+    let newArray = Box::new([10, 25, 1, 5, 90, 55, 14]);
+    let sorted = sort_array(newArray);
+    for x in sorted.iter() {
+        println!("{}", x);
+    }
 }
