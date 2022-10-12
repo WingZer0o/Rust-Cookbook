85c22ade4f66291745ab283617493541deb4d48f
Mike Mulchrone <mtmulch0191@outlook.com>
2022-10-11 21:54:27 -0400

    adding boxing to reclaim memory when the boxes are dropped

--- object-oriented-programming/src/main.rs
+++ object-oriented-programming/src/main.rs
@@ -3,6 +3,7 @@ use trait_file::Show;
 
 fn main() {
     big_implications();
+    big_box_implications();
 }
 
 fn big_implications() {
@@ -10,6 +11,16 @@ fn big_implications() {
     let maybe_pi = 3.14;
     let v: Vec<&dyn Show> = vec![&answer,&maybe_pi];
     for d in v.iter() {
+        println!("show {}", d.show());
+    }
+}
+
+// 
+fn big_box_implications() {
+    let answer = Box::new(42);
+    let maybe_pi = Box::new(3.14);
+    let show_list: Vec<Box<dyn Show>> = vec![answer, maybe_pi];
+    for d in &show_list {
         println!("show {}", d.show());
     }
 }
